a43f5939ccabe48a9f2c76de8d661c215732b1e1
Adaobi-Chuks <chuksaginamada@gmail.com>
2025-01-10 09:35:31 +0100

    Docs: Add swagger config toapp and getuser

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+
+  // Apply validation pipe to application
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
     transform: true
   }));
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle("Blog API Documentation")
+    .setDescription("Use this documentation to understand how to interact with the API. Use the base API URL as http://localhost:3000")
+    .setTermsOfService("http://localhost:3000/terms-of-service")
+    .setLicense(
+      "MIT License",
+      "http://www.opensource.org/licenses/mit-license.html")
+    .setVersion("1.0")
+    .addServer("http://localhost:3000")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,8 +4,10 @@ import { CreateUserDto } from './dtos/create-user.dto';
 import { PatchUserDto } from './dtos/patch-user.dto';
 import { UserService } from './providers/users-service';
 import { GetUsersParamDto } from './dtos/get-users-params.dto';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
+@ApiTags("Users")
 export class UsersController {
 
     constructor(
@@ -13,6 +15,28 @@ export class UsersController {
     ){}
 
     @Get("/:id?")
+    @ApiOperation({
+        summary: "Fetches a list of registered users on the application",
+        description: "This endpoint returns a list of users based off the inputted query and params."
+    })
+    @ApiQuery({
+        name: "limit",
+        required: false,
+        type: Number,
+        description: "The number of users returned per query.",
+        example: 10
+    })
+    @ApiQuery({
+        name: "page",
+        required: false,
+        type: Number,
+        description: "The page number you want the api to return.",
+        example: 1
+    })
+    @ApiResponse({
+        status: 200,
+        description: "The list of users was successfully returned."
+    })
     public getUsers(
         @Param() params: GetUsersParamDto,
         @Query("limit", new DefaultValuePipe(10), ParseIntPipe) limit: number | undefined,
