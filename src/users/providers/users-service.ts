855b3c80457d50da75585202789d61d8df8ae0dc
Adaobi-Chuks <chuksaginamada@gmail.com>
2025-01-14 15:31:46 +0100

    Feat: configservice for env

--- src/users/providers/users-service.ts
+++ src/users/providers/users-service.ts
@@ -5,6 +5,7 @@ import { Repository } from "typeorm";
 import { User } from "../user.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { CreateUserDto } from "../dtos/create-user.dto";
+import { ConfigService } from "@nestjs/config";
 
 /**
  * Class to connect to users table and perform business operations
@@ -17,7 +18,9 @@ export class UserService {
         private userRepository: Repository<User>,
 
         @Inject(forwardRef(() => AuthService))
-        private readonly authService: AuthService
+        private readonly authService: AuthService,
+    
+        private readonly configService: ConfigService
     ) { }
 
     public async createUser(user: CreateUserDto) {
@@ -44,6 +47,9 @@ export class UserService {
         limit: number,
         page: number
     ) {
+
+        const envTest = this.configService.get<string>("S3_BUCKET_NAME");
+        console.log(envTest);
 
         const isAuth = this.authService.isAuthenticated();
         console.log(isAuth);
